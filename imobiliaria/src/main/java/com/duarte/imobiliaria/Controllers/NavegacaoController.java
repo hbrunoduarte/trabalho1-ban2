edb4336fe80b3589f594b2cfaf26c1d4ee497530
João Víctor Paiva Nunes Pereira <joaov.pnp@gmail.com>
2026-09-13 14:21:12 -0300

    Estruturando melhor o projeto

--- imobiliaria/src/main/java/com/duarte/imobiliaria/Controllers/NavegacaoController.java
+++ imobiliaria/src/main/java/com/duarte/imobiliaria/Controllers/NavegacaoController.java
@@ -1,4 +1,4 @@
-package com.duarte.imobiliaria;
+package com.duarte.imobiliaria.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
